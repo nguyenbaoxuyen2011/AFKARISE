d75e1315d9aa367bb01c9be008bbe85b46a84135
Nguyễn Bảo Xuyên <81402237+nguyenbaoxuyen2011@users.noreply.github.com>
2025-03-31 22:34:39 +0700

    Update afk.lua

--- afk.lua
+++ afk.lua
@@ -1,16 +1,1 @@
-repeat task.wait() until game:IsLoaded()
-
-while task.wait() do
-    if game.PlaceId ~= 116614712661486 then
-        game:GetService("TeleportService"):Teleport(116614712661486)
-    end
-end
-
-local VirtualUser = game:service "VirtualUser"
-
-game:service("Players").LocalPlayer.Idled:connect(
-	function()
-		VirtualUser:CaptureController()
-		VirtualUser:ClickButton2(Vector2.new())
-	end
-)
+uj
