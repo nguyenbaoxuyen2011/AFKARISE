997d30b8aff80bc12dee405a3cd25ba1b73fd10c
Nguyễn Bảo Xuyên <81402237+nguyenbaoxuyen2011@users.noreply.github.com>
2025-04-01 09:36:42 +0700

    Update afk.lua

--- afk.lua
+++ afk.lua
@@ -1,4 +1,4 @@
-repeat task.wait() until game:IsLoaded()
+/*repeat task.wait() until game:IsLoaded()
 
 while task.wait() do
     if game.PlaceId ~= 116614712661486 then
@@ -14,3 +14,4 @@ game:service("Players").LocalPlayer.Idled:connect(
 		VirtualUser:ClickButton2(Vector2.new())
 	end
 )
+*/
